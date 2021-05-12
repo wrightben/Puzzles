0708152d288dcb94b475fee911af3b53b831c976
wrightben <mail@wrightben.com>
2021-05-12 11:04:37 -0400

    gitall commit

---
+++ word search/word_search.py
@@ -0,0 +1,95 @@
+import math
+
+puzzle = "fibbgnrxbxzfucioepflcabpkrceiyxnelnwltobbxduohkgmqecmnvqaabmulmdbctkjuixhwzsnnysukrrejzeljzrmvkljcgdhuzmtgniripsnijracellfpitsycisweetrgykgyxopreyzdeowwftjucdftdleolrrzrpynehobudqwitunayronqudueteyvoflsmasxyphomacfilihulylgifbejvlttptrbegazfnvptannquzrndaqrcanmrcqtrasifpsntthtkvzdeweiaoijuhrhzobrxwbumtwhnjgpglifgwsyljsmbgroxldhvxnhldehlhhcolorfuloqtofpirskrkssqlbtolhejspaodjsvorugekaaisialidgaykmiioizzxorcfvihqvrvoweiafnubnekeliqmrxmgbwtblugwdsqrirfhdordlxacmucbkcfxolunwglierionvaeeqvjixeczvfvljunnslplrkjkalsyoaedheeiofspaakcgoloocdczmzmhvffdgicehhazyxkitxjupuqjdygtnfosjxspwzcbwfghgbywwlylevolkugxbtbcfngefyzzsupajaqvm"
+puzzleAsList = list( puzzle )
+puzzleLength = len( puzzle )
+puzzleSize = int(math.sqrt( puzzleLength ))
+puzzleIndex = {
+	'a' : [ ], 'b' : [ ], 'c' : [ ], 'd' : [ ], 'e' : [ ], 'f' : [ ], 'g' : [ ], 'h' : [ ], 'i' : [ ], 
+	'j' : [ ], 'k' : [ ], 'l' : [ ], 'm' : [ ], 'n' : [ ], 'o' : [ ], 'p' : [ ], 'q' : [ ], 'r' : [ ], 
+	's' : [ ], 't' : [ ], 'u' : [ ], 'v' : [ ], 'w' : [ ], 'x' : [ ], 'y' : [ ], 'z' : [ ]
+}
+
+# INDEX letters of the puzzle
+i = 0
+for l in puzzleAsList:
+	puzzleIndex[l].append( i )
+	i += 1
+
+
+# FUNCTIONS
+def getIndex(r,c,s):
+	return ( (r-1) * s ) + c - 1
+
+def getCol(i,s):
+	return math.ceil( abs(i) % s ) + 1
+
+def getRow(i,s):
+	return math.ceil( (abs(i) + 1) / s )
+	
+def up(i,w,s):
+	wl = len(w)
+
+	# e-tests
+	e = i - (s * (wl - 1))
+	if e < 0: return 0
+	if puzzle[e] != w[-1]: return 0
+	_up = list(puzzle[x] for x in range(i,e-s,-s) )
+	
+	if list(w) == _up: return 1
+	
+	return 0
+
+def down(i,w,s):
+	wl = len(w)
+	
+	# e-tests
+	e = i + (s * (wl - 1))
+	if e >= puzzleLength: return 0
+	if puzzle[e] != w[-1]: return 0
+
+	_down = list(puzzle[x] for x in range(i,e+s,s) )
+	
+	if list(w) == _down: return 1
+	
+	return 0
+
+
+
+
+# Words and Search
+
+words = [ # 34
+	"beautiful",
+	"bright",
+	"brilliant",
+	"brisk",
+	"chilly",
+	"clear",
+	"cold",
+	"colorful",
+	"cool",
+	"dew",
+	"early",
+	"extraordinary",
+	"fine",
+	"fresh",
+	"glorious",
+	"golden",
+	"hazy",
+	"inspiring",
+	"lovely",
+	"soft",
+	"splendid",
+	"sunny",
+	"sweet",
+	"warm"
+]
+
+
+for word in words:
+	for i in puzzleIndex[ word[0] ]:
+		print( 	word, i, 
+			up(i, word, puzzleSize),
+			down(i, word, puzzleSize)
+		)
